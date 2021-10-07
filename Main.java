95bd95610c0de07ec8fbc5a2eeba0131603904c9
Gamergrub <>
2021-10-07 21:20:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+/*this is a comment*/
+//This is another way to comment.
+//Java variable types
+You have 5 types of variables.
+Boolean-True/False 
+integers- int - whole numbers
+strings- str - text and words
+floats- decimials - 19.999999999, 4.9371279732
+Char-a b c d e f g h i k l m n o p      
